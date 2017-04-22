3001ded24e0e5e1ed528e857904b7437ac9ce6da
Mohamed Qamhieh <qamhieh@harri.com>
2017-04-22 11:13:27 +0300

    Sub Menu + Slider

--- example2/scripts/main.js
+++ example2/scripts/main.js
@@ -1,3 +1,12 @@
 /**
  * Created by Mohamed on 4/1/2017.
  */
+$(document).ready(function() {
+    $(".slider .slide_show").slick({
+        infinite: false,
+        slidesToShow: 1,
+        dots: true,
+        prevArrow: "<i class='fa fa-chevron-left slick-prev'></i>",
+        nextArrow: "<i class='fa fa-chevron-right slick-next'></i>",
+    });
+});
