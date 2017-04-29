92e92dbc678b5a3b7e5e40c6a07ded37281c8597
Mohamed Qamhieh <qamhieh@harri.com>
2017-04-29 10:21:16 +0300

    Accordion

--- example2/scripts/main.js
+++ example2/scripts/main.js
@@ -18,4 +18,20 @@ $(document).ready(function() {
             $(this).parents(".checkbox").removeClass("checked");
         }
     });
+
+    $(".accordion .title").on("click", function () {
+        var wasOpen = $(this).parents(".accordion").hasClass('open');
+        $(".accordion.open .body").slideUp(300);
+        $(".accordion.open").removeClass('open');
+
+        if (wasOpen) {
+            $(this).parents(".accordion").removeClass('open');
+            $(this).parents(".accordion").find(".body").slideUp(300);
+        }
+        else {
+            $(this).parents(".accordion").addClass('open');
+            $(this).parents(".accordion").find(".body").slideDown(150);
+        }
+
+    });
 });
